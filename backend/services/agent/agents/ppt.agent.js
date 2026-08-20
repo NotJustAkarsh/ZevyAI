a197cc13518c8a0f3af404574dbde459180e543e
Akarsh Singh <atechnical8@gmail.com>
2026-08-20 23:29:12 +0530

    Presentation Agent Added

--- backend/services/agent/agents/ppt.agent.js
+++ backend/services/agent/agents/ppt.agent.js
@@ -1,3 +1,76 @@
-export const pptAgent = async (req, res) => {
-    
-}
+import { getModel } from "../config/llmModels.js";
+import { generatePpt } from "../utils/generatePpt.js";
+import { getFromS3 } from "../utils/getFromS3.js";
+import { uploadToS3 } from "../utils/uploadToS3.js";
+
+export const pptAgent = async (state) => {
+  try {
+    const llm = await getModel("ppt");
+    const prompt = `You are a professional presentation designer.
+        
+        Return ONLY valid JSON.
+        
+        Format:
+        
+        {
+        "title":"",
+        "subtitle":"",
+        "slides:[
+        {
+        "title":"",
+        "points":[
+        "","","",""
+        ]
+        }
+        ]
+        }
+        
+        Rules:
+        
+        - Generate exactly 6 content slides.
+        - Each slid should have 4-6 concise bullet points.
+        - No markdown.
+        - No explanation.
+        - No code block.
+        - Return ONLY JSON.
+        
+        Topic:
+        
+        ${state.prompt}`;
+
+    const res = await llm.invoke(prompt);
+    const data = JSON.parse(res.content);
+    const ppt = await generatePpt(data);
+    const buffer = await ppt.write({
+      outputType: "nodebuffer",
+    });
+
+    const filename = `ppt-${Date.now()}.pptx`;
+
+    await uploadToS3(
+      filename,
+      buffer,
+      "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+    );
+
+    const downloadUrl = await getFromS3(filename, 24 * 60 * 60);
+
+    return {
+      ...state,
+      aiResponse: `# ✅ Presentation Generated
+
+**${data.title}**
+
+[📥 Download Presentation](${downloadUrl})
+
+_Link expires in 10 minutes._
+`,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      ...state,
+      aiResponse: "❌ Failed to Generate Presentation",
+    };
+  }
+};
